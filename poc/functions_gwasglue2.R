26146689c734cba944760099e57758464c723575
Rita Rasteiro <rita.rasteiro@gmail.com>
2022-08-03 13:11:47 +0100

    update functions

--- poc/functions_gwasglue2.R
+++ poc/functions_gwasglue2.R
@@ -1,4 +1,10 @@
-# 28/07/2022
+#28/07/2022  created
+#29/07/2022
+    # annotate function updated to allow more than one type of analysis_type,
+    # 1:length() replaced by seq_along()
+    # some spaces and lint solved
+    #  using Roxygen comments for documenting functions
+
 
 library(magrittr)
 library(dplyr)
@@ -7,42 +13,41 @@ library(ieugwasr)
 ######################################################################################
 # GWASGLUE2 FUNCTIONS
 
-# CLUMPED HITS
-#  @param clump Inherited from ieugwasr
-#     If clump=FALSE, extracts the data without clumping
-# @param source By default, OpenGWAS, Inherited from ieugwasr. 
-#     access_token Google OAuth2 access token. Used to authenticate level of access to data.
-# @return snps Array of variants
-clumped_hits <- function(traits,  clump = TRUE,source = ieugwasr::check_access_token()){
+#' CLUMPED HITS
+#' @param clump Inherited from ieugwasr
+#'     If clump=FALSE, extracts the data without clumping
+#' @param source By default, OpenGWAS, Inherited from ieugwasr. 
+#'     access_token Google OAuth2 access token. Used to authenticate level of access to data.
+#' @return snps Array of variants
+clumped_hits <- function(traits,  clump = TRUE, source = ieugwasr::check_access_token()) {
     # ch  <- ieugwasr::tophits(traits[1], clump=clump, source=source) 
         
     #use this line code instead if there is an "unused argument (source = source)" error. It happens if the access was already opened
-    ch  <- ieugwasr::tophits(traits[1], clump=clump)
+    ch  <- ieugwasr::tophits(traits[1], clump = clump)
     snps <- ch$rsid
     return(snps)
 }
 
-# PHEWAS_ids
-# @param variants 
-# @param variants Array of variants 
-# @param pval p-value threshold. Default = `0.00001`. Iherited from ieugwasr::phewas
-# @param batch Vector of batch IDs to search across. If `c()` (default) then returns all batches. Iherited fromieugwasr::phewas
-# @return id_list Array with traits ids
-phewas_ids <- function(variants, batch, pval){
-    id_list <- ieugwasr::phewas(pval = pval, variants=variants, batch=batch)$id %>%
+#' PHEWAS_ids
+#' @param variants Array of variants
+#' @param pval p-value threshold. Default = `0.00001`. Iherited from ieugwasr::phewas
+#' @param batch Vector of batch IDs to search across. If `c()` (default) then returns all batches. Iherited fromieugwasr::phewas
+#' @return id_list Array with traits ids
+phewas_ids <- function(variants, batch, pval) {
+    id_list <- ieugwasr::phewas(pval = pval, variants = variants, batch = batch)$id %>%
         unique()
     return(id_list)
 }
 
-# CREATE DATASET
-# @param traits An array with the study ids (eg. "ieu-a-2")
-# @param variants An array of variants to be analysed
-# @return dataset List of dataframes of summary statistics. 
-create_dataset <- function (traits, variants){
-       data_set <- list() 
-   for (i in 1:length(traits)) {
+#' CREATE DATASET
+#' @param traits An array with the study ids (eg. "ieu-a-2")
+#' @param variants An array of variants to be analysed
+#' @return dataset List of dataframes of summary statistics.
+create_dataset <- function(traits, variants) {
+       data_set <- list()
+   for (i in seq_along(traits)) {
 
-        data_set[[i]] <-  ieugwasr::associations(variants=variants, id=traits[i])
+        data_set[[i]] <-  ieugwasr::associations(variants = variants, id = traits[i])
         names(data_set)[i] <-  unique(data_set[[i]]$id)
     }
     # source ("harmonise.R") #wip: this is modified from the harmonise function of TwoSampleMR
@@ -52,35 +57,38 @@ create_dataset <- function (traits, variants){
 
 
 
-# ANNOTATE function (add extra columns to data_set)
-# @param data_set Output of create_dataset function()
-# @params exposure and outcome  For now it must be the dataset$id
-# @param ld_ref Name of the reference dataset to be used to build the LD matrix
-    # (eg "EUR" on OpenGWAS or if using locally corresponds to the prefix of the plink files)
-# @param Type of analysis  Label to be used the the convert tools functions (eg mr, finemap, coloc, etc) 
-# @return data_set A list of annotated dataframes
-annotate <- function (data_set, exposure=NULL, outcome=NULL, analysis_type=NULL, ld_ref=NULL){
+#' ANNOTATE function (add extra columns to data_set)
+#' @param data_set Output of create_dataset function()
+#' @params exposure and outcome  For now it must be the dataset$id
+#' @param ld_ref Name of the reference dataset to be used to build the LD matrix
+#' (eg "EUR" on OpenGWAS or if using locally corresponds to the prefix of the plink files)
+#' @param analysis_type  Array of labels used for the convert tools functions (eg mr, finemap, coloc, etc)
+#'  It creates the same number of columns as the length of the array (Eg, analysis_type1, analysis_type2, etc)
+#' @return data_set A list of annotated dataframes
+annotate <- function(data_set, exposure = NULL, outcome = NULL, analysis_type = NULL, ld_ref = NULL) {
     # sweep through the data_sets
-    for (i in 1:length(data_set)) {
+    for (i in seq_along(data_set)) {
 
         # specific for MR analysis
-        if (!is.null(exposure) & !is.null(outcome)){
+        if (!is.null(exposure) && !is.null(outcome)) {
             # we named previously each dataset with $id (see create_dataset ())
-            if (names(data_set)[i] == exposure){
+            if (names(data_set)[i] == exposure) {
                 data_set[[i]]$exposure <- exposure
             } else {
                 data_set[[i]]$outcome <- outcome
             }
         }
 
-        # LD reference data_set name 
-        if (!is.null(ld_matrix)){
+        # LD reference data_set name
+        if (!is.null(ld_matrix)) {
         data_set[[i]]$ld_ref <- ld_ref
         }
 
         # Type of analysis
-        if (!is.null(analysis_type)){
-        data_set[[i]]$analysis_type <- analysis_type
+        if (!is.null(analysis_type)) {
+            for (j in seq_along(analysis_type)) {
+            data_set[[i]][, paste0("analysis_type", j)] <- analysis_type[j]
+            }
         }
      }
     return(data_set)
