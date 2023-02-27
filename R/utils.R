55ecd7af34278524c89bad634932ac3bce02028e
Rita Rasteiro <rita.rasteiro@gmail.com>
2023-02-27 11:51:34 +0000

    update susieR wrapper

--- R/utils.R
+++ R/utils.R
@@ -33,3 +33,17 @@ merge_datasets <- function(...) {
 
 
 
+# @title Utility function to display warning messages as they occur (from susieR)
+# @param ... warning message
+# @param style either "warning" or "hint"
+#'@importFrom crayon combine_styles
+warning_message = function(..., style=c("warning", "hint")) {
+  style = match.arg(style)
+  if (style=="warning" && getOption("warn")>=0) {
+    alert <- combine_styles("bold", "underline", "red")	
+    message(alert("WARNING:"), " ", ...)
+  } else {
+    alert <- combine_styles("bold", "underline", "magenta")	
+    message(alert("HINT:"), " ", ...)
+  }
+}
