cd6612d389c401e5a0ddd8bd3ab9ff3c2639ead9
Rita Rasteiro <rita.rasteiro@gmail.com>
2023-06-12 15:34:03 +0100

    LD harmonisation updated
    * added ld_matrix_local() based on ieugwasr::ld_matrix_local() to R/harmonise_ld.R. It queries plink bedfiles by chr:pos instead of rsids using the plink --extract range option.  The LD matrix row and colnames are the variantids, that are created by the R/harmonise_ld.R:create_variantid_plink()
    * changed R/harmonise_ld.R:harmonise_ld_dat to accept variantids
    * changes to R/harmoniseLD_method.R:buildLDMatrix to use R/harmonise_ld.R:ld_matrix_local()
    * changes to R/harmoniseLD_method.R:harmoniseLDMatrix() to accept variantids
    updated R/harmoniseLD_method.R:harmonise_ld()

--- R/harmoniseLD_method.R
+++ R/harmoniseLD_method.R
@@ -1,20 +1,23 @@
+
 setGeneric("buildLDMatrix", function(object, ...) standardGeneric("buildLDMatrix"))
-setMethod("buildLDMatrix", "DataSet", function(object, ld_ref = NULL, pop = FALSE, bfile = FALSE, plink_bin = NULL){
+setMethod("buildLDMatrix", "DataSet", function(object, bfile = NULL, plink_bin = NULL){
+  
+  message("Building LD matrix")
+variants<-cbind(object@summary_sets[[1]]@ss$chr, object@summary_sets[[1]]@ss$position, object@summary_sets[[1]]@ss$position, 1:nrow(object@summary_sets[[1]]@ss))
+    
+  object@ld_matrix <- ld_matrix_local(variants, bfile=bfile, plink_bin=plink_bin)
 
-     message("Building LD matrix")
-      if (pop == TRUE){
-        object@ld_matrix <- suppressWarnings(ieugwasr::ld_matrix(object@summary_sets[[1]]@ss$rsid, pop = ld_ref, with_alleles = TRUE, bfile = FALSE,plink_bin = plink_bin))
-      }
-      if (bfile == TRUE){
-        object@ld_matrix <- suppressWarnings(ieugwasr::ld_matrix(object@summary_sets[[1]]@ss$rsid, pop=FALSE, with_alleles=TRUE, bfile=ld_ref, plink_bin=plink_bin))
-      }
-
-      rsid_avail <- rownames(object@ld_matrix)
-      message("\nData available for ", length(rsid_avail), " variants")
+  variants_avail <- rownames(object@ld_matrix)
+  message("\nData available for ", length(variants_avail), " variants")
 
   return(object)
 }
 )
+
+
+
+
+
 
 
 # Get methods for LDMatrix
@@ -37,15 +40,16 @@ setMethod("isHarmonisedLD","DataSet",function(object) {
 setGeneric("harmoniseLDMatrix", function(object) standardGeneric("harmoniseLDMatrix"))
 setMethod( "harmoniseLDMatrix", "DataSet", function(object) {
   
-  rsid_avail <- do.call(rbind, strsplit(rownames(object@ld_matrix), split="_"))[,1]
+  variants_avail <- do.call(rbind, strsplit(rownames(object@ld_matrix), split="\\|"))[,1]
+
   message("Gwasglue is now harmonising the SummarySets against the LD matrix")
   for (i in seq_along(object@summary_sets)){
     # message("Gwasglue is now harmonising ", object@summary_sets[[i]]@metadata$id, " the against LD matrix!")
  
     # subseting
-    sub_ss <- subset(object@summary_sets[[i]]@ss, object@summary_sets[[i]]@ss$rsid %in% rsid_avail)
+    sub_ss <- subset(object@summary_sets[[i]]@ss, object@summary_sets[[i]]@ss$variantid %in% variants_avail)
     
-    index <- match(object@summary_sets[[i]]@ss$rsid, rsid_avail)
+    index <- match(object@summary_sets[[i]]@ss$variantid, variants_avail)
     ld <- object@ld_matrix[index, index]
 
 
@@ -65,21 +69,95 @@ setMethod( "harmoniseLDMatrix", "DataSet", function(object) {
 )
 
 
-
 #' Harmonise data against LD matrix
 #' 
-#' Function to create a LDmatrix gwasglue2 object and set the @slot ld_matrix using ieugwasr::ld_matrix()
+#' Function to create a LDmatrix gwasglue2 object and set the @slot ld_matrix u
 #' @param dataset The DataSet gwasglue2 object
-#' @param ld_ref If bfile = TRUE, corresponds to the path and prefix of the plink files used to build the LD correlation matrix. If pop = TRUE, it corresponds to the population code in ieugwasr (eg. EUR) instead (default NULL). 
-#' @param pop logical (default FALSE)
-#' @param bfile logical (default FALSE)
+#' @param bfile It corresponds to the path and prefix of the plink files used to build the LD correlation matrix. 
 #' @param plink_bin Path to the plink executable
 #' @return  The DataSet gwasglue2 object harmonised
 #' @export 
-harmonise_ld <- function(dataset, ld_ref = NULL, pop = FALSE, bfile = FALSE, plink_bin = NULL){
+harmonise_ld <- function(dataset, bfile = NULL, plink_bin = NULL){
 
-    ld <- buildLDMatrix(dataset, ld_ref = ld_ref, pop = pop, bfile = bfile, plink_bin = plink_bin) 
+    ld <- buildLDMatrix(dataset, bfile = bfile, plink_bin = plink_bin) 
     
     ds <- harmoniseLDMatrix(ld)
     return(ds)
 }
+
+
+
+
+
+############################
+
+
+# setGeneric("buildLDMatrix_old", function(object, ...) standardGeneric("buildLDMatrix_old"))
+# setMethod("buildLDMatrix_old", "DataSet", function(object, ld_ref = NULL, pop = FALSE, bfile = FALSE, plink_bin = NULL){
+
+#      message("Building LD matrix")
+#       if (pop == TRUE){
+#         object@ld_matrix <- suppressWarnings(ieugwasr::ld_matrix(object@summary_sets[[1]]@ss$rsid, pop = ld_ref, with_alleles = TRUE, bfile = FALSE,plink_bin = plink_bin))
+#       }
+#       if (bfile == TRUE){
+#         object@ld_matrix <- suppressWarnings(ieugwasr::ld_matrix(object@summary_sets[[1]]@ss$rsid, pop=FALSE, with_alleles=TRUE, bfile=ld_ref, plink_bin=plink_bin))
+#       }
+
+#       rsid_avail <- rownames(object@ld_matrix)
+#       message("\nData available for ", length(rsid_avail), " variants")
+
+#   return(object)
+# }
+# )
+
+
+
+
+# setGeneric("harmoniseLDMatrixold", function(object) standardGeneric("harmoniseLDMatrixold"))
+# setMethod( "harmoniseLDMatrixold", "DataSet", function(object) {
+  
+#   rsid_avail <- do.call(rbind, strsplit(rownames(object@ld_matrix), split="_"))[,1]
+#   message("Gwasglue is now harmonising the SummarySets against the LD matrix")
+#   for (i in seq_along(object@summary_sets)){
+#     # message("Gwasglue is now harmonising ", object@summary_sets[[i]]@metadata$id, " the against LD matrix!")
+ 
+#     # subseting
+#     sub_ss <- subset(object@summary_sets[[i]]@ss, object@summary_sets[[i]]@ss$rsid %in% rsid_avail)
+    
+#     index <- match(object@summary_sets[[i]]@ss$rsid, rsid_avail)
+#     ld <- object@ld_matrix[index, index]
+
+
+#     h <- harmonise_ld_dat(sub_ss,ld)
+#     object@summary_sets[[i]]@ss <- h[[1]]
+    
+
+#     message("\nThere are ", dim(h[[1]])[1], " variants remaining after harmonising ", object@summary_sets[[i]]@metadata$id, " against the LD matrix.")
+#     }
+  
+#   object@ld_matrix <- h[[2]]
+#   object@is_harmonisedLD <- TRUE
+  
+#   message("Done!")
+#   return(object)
+# }
+# )
+
+
+# #' Harmonise data against LD matrix
+# #' 
+# #' Function to create a LDmatrix gwasglue2 object and set the @slot ld_matrix using ieugwasr::ld_matrix()
+# #' @param dataset The DataSet gwasglue2 object
+# #' @param ld_ref If bfile = TRUE, corresponds to the path and prefix of the plink files used to build the LD correlation matrix. If pop = TRUE, it corresponds to the population code in ieugwasr (eg. EUR) instead (default NULL). 
+# #' @param pop logical (default FALSE)
+# #' @param bfile logical (default FALSE)
+# #' @param plink_bin Path to the plink executable
+# #' @return  The DataSet gwasglue2 object harmonised
+# #' @export 
+# harmonise_ld <- function(dataset, ld_ref = NULL, pop = FALSE, bfile = FALSE, plink_bin = NULL){
+
+#     ld <- buildLDMatrix(dataset, ld_ref = ld_ref, pop = pop, bfile = bfile, plink_bin = plink_bin) 
+    
+#     ds <- harmoniseLDMatrix(ld)
+#     return(ds)
+# }
--- R/harmonise_ld.R
+++ R/harmonise_ld.R
@@ -1,28 +1,94 @@
-# WIP
-# harmonise_ld_dat() is based TwoSampleMR::harmonise_ld_dat()
+#' modified from ieugwasr::ld_matrix_local ()
+#' Get LD matrix using local plink binary and reference dataset
+#'
+#' @param variants List of variants (in plink 'set range' format). 
+#' @param bfile Path to bed/bim/fam ld reference panel
+#' @param plink_bin Specify path to plink binary. Default = `NULL`. 
+#' @importFrom utils read.table write.table
+#'
+#' @return data frame
+ld_matrix_local <- function(variants, bfile, plink_bin)
+{
+	# Make textfile
+	shell <- ifelse(Sys.info()['sysname'] == "Windows", "cmd", "sh")
+	fn <- tempfile()
+	write.table(data.frame(variants), file=fn, row.names=F, col.names=F, quote=F)
+
+	
+	fun1 <- paste0(
+		shQuote(plink_bin, type=shell),
+		" --bfile ", shQuote(bfile, type=shell),
+		" --extract range ", shQuote(fn, type=shell), 
+		" --make-just-bim ", 
+		" --keep-allele-order ",
+		" --out ", shQuote(fn, type=shell)
+	)
+	system(fun1)
+
+	bim <- read.table(paste0(fn, ".bim"), stringsAsFactors=FALSE)
+
+	fun2 <- paste0(
+		shQuote(plink_bin, type=shell),
+		" --bfile ", shQuote(bfile, type=shell),
+		" --extract range ", shQuote(fn, type=shell), 
+		" --r square ", 
+		" --keep-allele-order ",
+		" --out ", shQuote(fn, type=shell)
+	)
+	system(fun2)
+	res <- read.table(paste0(fn, ".ld"), header=FALSE) %>% as.matrix
+	rownames(res) <- colnames(res) <- paste(create_variantid_plink(bim), bim$V5, bim$V6, sep="|")
+	
+	return(res)
+}
 
 
-#Harmonise LD matrix against summary data
+create_variantid_plink <-function(bim_file) {
+  bim <- bim_file
+  nvariants <- dim(bim)[1] 
+  variantid <- lapply(1:nvariants, function(i){
+    x <- sort(c(bim[i,]$V5,bim[i,]$V6))
+    if (nchar(x[1]) > 10 || nchar(x[2]) <= 10){
+      id <- paste0(bim[i,]$V1,":", bim[i,]$V4,"_#",digest::digest(x[1],algo= "murmur32"),"_",x[2]) 
+    }
+    if (nchar(x[1]) <= 10 || nchar(x[2]) > 10){
+      id <- paste0(bim[i,]$V1,":", bim[i,]$V4,"_",x[1],"_#",digest::digest(x[2],algo= "murmur32")) 
+    }
+    if (nchar(x[1]) > 10 || nchar(x[2]) > 10){
+      id <- paste0(bim[i,]$V1,":", bim[i,]$V4,"_#",digest::digest(x[1],algo= "murmur32"),"_#",digest::digest(x[2],algo= "murmur32")) 
+    } else {
+      id <- paste0(bim[i,]$V1,":", bim[i,]$V4,"_",x[1],"_",x[2]) 
+    }
+  }) 
+
+  variantid <- unlist(variantid)
+  return(variantid)
+}
+
+
+
+#' Harmonise LD matrix against summary data
+#' harmonise_ld_dat() is based TwoSampleMR::harmonise_ld_dat()
+#'
+#  LD matrix returns with variantid_ea_oa identifiers. Make sure that they are oriented to the same effect allele as the summary dataset. Summary dataset can be dat1 dataset or harmonised dartaset
 #
-# LD matrix returns with rsid_ea_oa identifiers. Make sure that they are oriented to the same effect allele as the summary dataset. Summary dataset can be dat1 dataset or harmonised dartaset
-#
-# @param x harmonised dataset
-# @param ld Output from ld_matrix
-# @return List of dataset and harmonised LD matrix
+#' @param x harmonised dataset
+#' @param ld Output from ld_matrix
+#' @return List of dataset and harmonised LD matrix
 harmonise_ld_dat <- function(x, ld){
-	snpnames <- do.call(rbind, strsplit(rownames(ld), split="_"))
-	i1 <- snpnames[,1] %in% x$rsid
+	snpnames <- do.call(rbind, strsplit(rownames(ld), split="\\|"))
+	i1 <- snpnames[,1] %in% x$variantid
 	ld <- ld[i1,i1]
 	snpnames <- snpnames[i1,]
-	i2 <- x$rsid %in% snpnames[,1]
+	i2 <- x$variantid %in% snpnames[,1]
 	x <- x[i2,]
-	# stopifnot(all(snpnames[,1] == x$rsid))
+	# stopifnot(all(snpnames[,1] == x$variantid))
 	x$ea <- as.character(x$ea)
 	x$nea <- as.character(x$nea)
 	# Set1 x and ld alleles match
 	snpnames <- data.frame(snpnames, stringsAsFactors=FALSE)
-	snpnames <- merge(subset(x, select=c(rsid, ea, nea)), snpnames, by.x="rsid", by.y="X1")
-	snpnames <- snpnames[match(x$rsid, snpnames$rsid),]
+	snpnames <- merge(subset(x, select=c(variantid, ea, nea)), snpnames, by.x="variantid", by.y="X1")
+	snpnames <- snpnames[match(x$variantid, snpnames$variantid),]
 	snpnames$keep <- (snpnames$X2 == snpnames$ea & snpnames$X3 == snpnames$nea) |
 		(snpnames$X3 == snpnames$ea & snpnames$X2 == snpnames$nea)
 
@@ -35,20 +101,20 @@ harmonise_ld_dat <- function(x, ld){
 
 	if(any(!snpnames$keep))
 	{
-		message(" - the following SNPs could not be aligned to the LD reference panel: \n- ", paste(subset(snpnames, !keep)$rsid, collapse="\n - "))
+		message(" - the following SNPs could not be aligned to the LD reference panel: \n- ", paste(subset(snpnames, !keep)$variantid, collapse="\n - "))
 	}
 
 
 	snpnames$flip1 <- snpnames$X2 != snpnames$ea
-	x <- subset(x, rsid %in% snpnames$rsid)
+	x <- subset(x, variantid %in% snpnames$variantid)
 	temp1 <- x$ea[snpnames$flip1]
 	temp2 <- x$nea[snpnames$flip1]
 	x$beta[snpnames$flip1] <- x$beta[snpnames$flip1] * -1
 	x$ea[snpnames$flip1] <- temp2
 	x$nea[snpnames$flip1] <- temp1
 
-	rownames(ld) <- snpnames$rsid
-	colnames(ld) <- snpnames$rsid
+	rownames(ld) <- snpnames$variantid
+	colnames(ld) <- snpnames$variantid
 
 	if(any(!snpnames$keep))
 	{
