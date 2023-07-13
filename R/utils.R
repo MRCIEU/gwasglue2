3b3411a967b9c6ff3bcd61dd2cb94710dde0884f
Rita Rasteiro <rita.rasteiro@gmail.com>
2023-07-13 12:08:58 +0100

    remove crayon dependency

--- R/utils.R
+++ R/utils.R
@@ -4,17 +4,17 @@
  "%ni%" <- Negate("%in%")
 
 
-# @title Utility function to display warning messages as they occur (from susieR)
-# @param ... warning message
-# @param style either "warning" or "hint"
-#'@importFrom crayon combine_styles
-warning_message = function(..., style=c("warning", "hint")) {
-  style = match.arg(style)
-  if (style=="warning" && getOption("warn")>=0) {
-    alert <- combine_styles("bold", "underline", "red")	
-    message(alert("WARNING:"), " ", ...)
-  } else {
-    alert <- combine_styles("bold", "underline", "magenta")	
-    message(alert("HINT:"), " ", ...)
-  }
-}
+# # @title Utility function to display warning messages as they occur (from susieR)
+# # @param ... warning message
+# # @param style either "warning" or "hint"
+# #'@importFrom crayon combine_styles
+# warning_message = function(..., style=c("warning", "hint")) {
+#   style = match.arg(style)
+#   if (style=="warning" && getOption("warn")>=0) {
+#     alert <- combine_styles("bold", "underline", "red")	
+#     message(alert("WARNING:"), " ", ...)
+#   } else {
+#     alert <- combine_styles("bold", "underline", "magenta")	
+#     message(alert("HINT:"), " ", ...)
+#   }
+# }
