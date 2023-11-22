930f3adf6c250f28f12af7adb25669f48e4f70fc
Rita Rasteiro <rita.rasteiro@gmail.com>
2023-11-22 10:58:54 +0000

    yaml vignette

--- R/convertTo.R
+++ R/convertTo.R
@@ -52,3 +52,86 @@ setMethod("convertForTwoSampleMR", "DataSet", function(dataset) {
   return(dataset)
 }
 )
+
+
+#' Convert tool to TwoSampleMR format
+#' @description Converts SummarySets within a Dataset to a format that can be read by TwoSampleMR
+#' @param dataset The gwasglue2 DataSet object
+#' @return A TwoSampleMR data.frame input format
+#' @export
+#' @docType methods
+#' @rdname convertToTwoSampleMR
+setGeneric("convertToTwoSampleMR", function(dataset) standardGeneric("convertToTwoSampleMR"))
+#' @rdname convertToTwoSampleMR
+setMethod("convertToTwoSampleMR", "DataSet", function(dataset) {
+  # TODO add warnings for when the data  is already converted
+
+
+  message("Gwasglue is now converting the data to TwoSampleMR format!")
+  for (i in seq_along(dataset@summary_sets)){
+    # Exposure
+    if (dataset@summary_sets[[i]]@attributes$mr_label == "exposure"){
+      
+      if(i==1){
+          # change column names
+      mr_exposure <- dplyr::rename(dataset@summary_sets[[i]]@ss, c(SNP = rsid,effect_allele=ea, other_allele = nea, pval=p, samplesize=n)) # nolint
+      #  add extra column named "mr_keep"
+      mr_exposure$mr_keep <- !is.na( mr_exposure$beta) &
+        !is.na( mr_exposure$se) &
+        !is.na( mr_exposure$effect_allele) &
+        !is.na( mr_exposure$SNP)
+
+        c <- colnames(mr_exposure) %ni% c("SNP","mr_keep")
+        names(mr_exposure)[c] <- paste0(colnames(mr_exposure[c]), ".exposure")
+        #  add extra column named "exposure"
+        mr_exposure$exposure <- "exposure" #TODO change how to fill this column?
+      } else{
+         ss <- dplyr::rename(dataset@summary_sets[[i]]@ss, c(SNP = rsid,effect_allele=ea, other_allele = nea, pval=p, samplesize=n)) # nolint
+      #  add extra column named "mr_keep"
+        ss$mr_keep <- !is.na(ss$beta) &
+        !is.na( ss$se) &
+        !is.na(ss$effect_allele) &
+        !is.na(ss$SNP)
+        c <- colnames(ss) %ni% c("SNP","mr_keep")
+        names(ss)[c] <- paste0(colnames(ss[c]), ".exposure")
+      
+      mr_exposure <- dplyr::bind_rows(mr_exposure,ss)
+      }
+    }
+
+    # Outcome
+    if (dataset@summary_sets[[i]]@attributes$mr_label == "outcome"){
+        
+      if(i==1){
+          # change column names
+      mr_outcome <- dplyr::rename(dataset@summary_sets[[i]]@ss, c(SNP = rsid,effect_allele=ea, other_allele = nea, pval=p, samplesize=n)) # nolint
+      #  add extra column named "mr_keep"
+      mr_outcome$mr_keep <- !is.na( mr_outcome$beta) &
+        !is.na( mr_outcome$se) &
+        !is.na( mr_outcome$effect_allele) &
+        !is.na( mr_outcome$SNP)
+
+        c <- colnames(mr_outcome) %ni% c("SNP","mr_keep")
+        names(mr_outcome)[c] <- paste0(colnames(mr_outcome[c]), ".outcome")
+        #  add extra column named "exposure"
+        mr_outcome$exposure <- "outcome" #TODO change how to fill this column?
+      } else{
+         ss <- dplyr::rename(dataset@summary_sets[[i]]@ss, c(SNP = rsid,effect_allele=ea, other_allele = nea, pval=p, samplesize=n)) # nolint
+      #  add extra column named "mr_keep"
+        ss$mr_keep <- !is.na(ss$beta) &
+        !is.na( ss$se) &
+        !is.na(ss$effect_allele) &
+        !is.na(ss$SNP)
+        c <- colnames(ss) %ni% c("SNP","mr_keep")
+        names(ss)[c] <- paste0(colnames(ss[c]), ".outcome")
+      
+      mr_outcome <- dplyr::bind_rows(mr_outcome,ss)
+      }
+    }
+
+  }
+
+  mr_input <- merge(mr_exposure,mr_outcome, by = c("SNP", "mr_keep")) 
+  return(mr_input)
+}
+)
--- R/gwasglue.R
+++ R/gwasglue.R
@@ -1,3 +1,138 @@
+#' gwasglue  (WIP)
+#' @param yaml A yaml R object.
+#' @param read A boolean indicating whether the yaml object is a path to a yaml file or a yaml R object. IF TRUE, yaml is a path.
+#' @param path_to_yaml A character vector indicating the path to the yaml file.
+#' @return A gwasglue2 DataSet object.
+#' @importFrom yaml read_yaml yaml.load
+gwasglue <- function(yaml, read=FALSE, path_to_yaml="config.yaml"){
+
+  # read or load yaml
+  if (isTRUE(read)) { yaml <- yaml::read_yaml(path_to_yaml) }
+  else { yaml <- yaml::yaml.load(yaml) }
+
+  # get nb of jobs
+  njobs <- length(yaml) -1 # -1 because of the analyses part
+ 
+  for(i in 1:njobs){
+    job <- yaml[[i]]
+        
+    # get variants
+    if(job$variants$tophits ==FALSE){
+      chr <- (strsplit(job$variants$variant_list, ":") [[1]][1] %>% strsplit(., "chr"))[[1]][2]
+      start_end <- strsplit(job$variants$variant_list, ":") [[1]][2]
+      variants <- paste0(chr,":",start_end)
+      }else{
+      exposures <- job$organization$lhs
+      variants <- get_tophits_from_datasource(exposures, summarydata=job$summarydata)
+      }
+
+ 
+    shape <- job$variants$shape
+  
+    # get the number of SummarySets and create a list
+    nsummary_sets <- length(job$summarydata)
+    summary_sets <- list()
+
+    for (j in 1:nsummary_sets){
+      # read the parameters for each SummarySet
+      source <- job$summarydata[[j]]$source
+      location <- job$summarydata[[j]]$location
+      build <- job$summarydata[[j]]$build
+        
+      if(source == "ieugwasr"){
+       data <- ieugwasr::associations(variants = variants, id = location)
+      }else{stop("source not supported yet")}
+
+      # create summarysets
+      summary_sets[[j]] <- create_summaryset(data,
+                      metadata = NULL,
+                      qc = FALSE,
+                      beta_col = "beta",
+                      se_col = "se",
+                      samplesize_col = "n",
+                      pvalue_col = "p",
+                      chr_col = "chr",
+                      position_col = "position",
+                      rsid_col = "rsid",
+                      effect_allele_col = "ea",
+                      other_allele_col = "nea",
+                      eaf_col = "eaf",
+                      id_col = "id",
+                      trait_col = "trait", 
+                      build = build)
+
+    }
+  
+if(yaml$analyses$type == "TwoSampleMR"){
+    #  TODO alow for more than 2 dataset
+#  WIP fix error
+      # create mr_input
+
+      
+     summary_sets[[1]] <- setAttributes(summary_sets[[1]], mr_label = "exposure")
+     summary_sets[[2]] <- setAttributes(summary_sets[[2]], mr_label = "outcome")
+      # create dataset
+      dataset <- create_dataset(summary_sets=summary_sets)
+      mr_input <- convertToTwoSampleMR(dataset)
+      return(mr_input)
+  }else{
+    # create dataset
+    dataset <- create_dataset(summary_sets=summary_sets)
+    # get lddata info and harmonise againts reference panel
+    if(!is.null(job$lddata)){
+    bfile <-job$lddata$location
+    plink_bin <- job$lddata$plink_bin
+    dataset <- harmonise_ld(dataset, bfile = bfile, plink_bin = plink_bin)
+  }
+
+    return(dataset)
+  }
+  
+  
+            
+  
+  
+  
+  
+
+  }
+  # TODO: For now, we only return the one job, one DataSet (to change this we should return a list of DataSets). But should we allow for more than 1 dataset be created by the gwasglue function?
+    
+}
+
+
+# get_variants <- function(){}
+
+# get_summarydata <- function(){}
+
+get_tophits_from_datasource <- function(exposures, summarydata){
+
+  exposure <- lapply(summarydata, \(s){
+    if(exposures %in% s$label){
+      return(s$location[which(exposures %in% s$label)])
+    }
+    # else{
+    #   message("Exposure label ", s, " not found in the YAML summarydata.")
+    # }
+    
+  }) %>% unlist()
+
+  tophits <- ieugwasr::tophits(exposure[1])$rsid
+
+  # TODO: deal with more than one exposure
+  return(tophits)
+ }
+
+# get_lddata <- function(){}
+
+# get_analyses <- function(){}
+
+# get_organisation <- function(){}
+
+
+# -------------------------------------------------------------------------
+
+
 #' YAML constructor (WIP)
 #' @param traits A character vector of traits to be analysed.
 #' @param region A character vector of regions to be analysed.
@@ -10,7 +145,7 @@
 #' @return A yaml object.
 #' @importFrom yaml write_yaml as.yaml
 
-plan_coloc <- function(traits, region, build = NULL, bfile = NULL, plink_bin = NULL, write = FALSE, outfile = "config.yaml") {
+job_coloc <- function(traits, region, build = NULL, bfile = NULL, plink_bin = NULL, write = FALSE, outfile = "config.yaml"){
   
   # read number of DataSets to  build and analyse TODO: should we allow for more than 1 dataset?
   ndatasets <- length(region)
@@ -31,82 +166,15 @@ plan_coloc <- function(traits, region, build = NULL, bfile = NULL, plink_bin = N
     return(l)
   })
    
-  names(out) <- paste0("plan", 1:length(region))
+  names(out) <- paste0("job", 1:length(region))
 
   # write yaml to file
-  if (isTRUE(write)){ yaml::write_yaml(out, outfile) }
-# return a yaml object
-  return(yaml::as.yaml(out))
-}
+  if (isTRUE(write)){yaml::write_yaml(out, outfile)}
+
+  # return a yaml object
+  out <- yaml::as.yaml(out)
+  return(out)
+}   
 
 
-#' gwasglue  (WIP)
-#' @param yaml A yaml R object.
-#' @param read A boolean indicating whether the yaml object is a path to a yaml file or a yaml R object. IF TRUE, yaml is a path.
-#' @param path_to_yaml A character vector indicating the path to the yaml file.
-#' @return A gwasglue2 DataSet object.
-#' @importFrom yaml read_yaml yaml_load
-gwasglue <- function(yaml, read=FALSE, path_to_yaml="config.yaml"){
-
-  # read or load yaml
-  if (isTRUE(read)) { yaml <- yaml::read_yaml(path_to_yaml) }
-  else { yaml <- yaml::yaml.load(yaml) }
-
-  # get nb of plans
-  nplans <- length(yaml)
- 
-  for(i in 1:nplans){
-    plan <- yaml[[i]]
-        
-    # get variants
-    chr <- (strsplit(plan$variants$variant_list, ":") [[1]][1] %>% strsplit(., "chr"))[[1]][2]
-    start_end <- strsplit(plan$variants$variant_list, ":") [[1]][2]
-    variants <- paste0(chr,":",start_end)
-  
-    # get the number of SummarySets and create a list
-    nsummary_sets <- length(plan$summarydata)
-    summary_sets <- list()
-
-    for (i in 1:nsummary_sets){
-      # read the parameters for each SummarySet
-      source <- plan$summarydata[[i]]$source
-      location <- plan$summarydata[[i]]$location
-      build <- plan$summarydata[[i]]$build
-        
-      if(source == "ieugwasr"){
-       data <- ieugwasr::associations(variants = variants, id = location)
-      }else{stop("source not supported yet")}
-
-    # create summarysets
-    summary_sets[[i]] <- create_summaryset(data,
-                    metadata = NULL,
-                    qc = FALSE,
-                    beta_col = "beta",
-                    se_col = "se",
-                    samplesize_col = "n",
-                    pvalue_col = "p",
-                    chr_col = "chr",
-                    position_col = "position",
-                    rsid_col = "rsid",
-                    effect_allele_col = "ea",
-                    other_allele_col = "nea",
-                    eaf_col = "eaf",
-                    id_col = "id",
-                    trait_col = "trait", 
-                    build = build)
-
-        }
-  
-  # get lddata info
-  bfile <-plan$lddata$location
-  plink_bin <- plan$lddata$plink_bin
-  
-  # create dataset
-  dataset <- create_dataset(summary_sets=summary_sets) %>% 
-            harmonise_ld(bfile = bfile, plink_bin = plink_bin)
-
-    return(dataset)
-  # TODO: For now, we only return the one plan, one DataSet (to change this we should return a list of DataSets). But should we allow for more than 1 dataset be created by the gwasglue function?
-    
-}
-
+# ------------------------------------------------------------------------
